16cde798637725deaa3e3ccd68d5df5c9b518564
ShajithTSL <shajith@tsl-me.com>
2026-04-07 08:27:59 +0000

    Server Commits

--- cyrix/hooks.py
+++ cyrix/hooks.py
@@ -93,7 +93,13 @@ jinja = {
 		"cyrix.custom_py.jinja.get_labour",
 		"cyrix.custom_py.jinja.get_material_cost",
 		"cyrix.custom_py.jinja.get_invoice_details",
-		"cyrix.custom_py.jinja.get_pi"
+		"cyrix.custom_py.jinja.get_pi",
+		"cyrix.custom_py.jinja.get_sales",
+		"cyrix.custom_py.jinja.sales_summary",
+		"cyrix.custom_py.jinja.weekly_report",
+
+		# "cyrix.cyrix_tsl.doctype.wo_approval.wo_approval.get_sales"
+
 	]
 }
 
